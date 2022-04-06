1ecc799bc2b22e666f323c31cf5ad43b8afdf400
Aanxhel <ang.core92@yahoo.com>
2022-04-06 16:07:56 -0500

    actualizacion java to sts

---
+++ homework-api-wipro/src/test/java/com/dev/springboot/backend/apirest/HomeworkApiWiproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.springboot.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApiWiproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
